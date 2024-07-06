88497c99742de60635e1b6646c5103fbb0218f4a
Maddalena Bozzola <maddalena.bozzola@itsrizzoli.it>
2024-07-06 11:02:52 +0200

    connessione al db

--- file_lavoro.py
+++ file_lavoro.py
@@ -1,1 +1,9 @@
 #file di lavoro
+
+from bson import ObjectId
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://maddalenabozzola_:1iEoHJKaFeMqZDHH@ufs13.p4dqcok.mongodb.net/?retryWrites=true&w=majority&appName=UFS13')
+
+db = client['biglietti']
+#print(db)

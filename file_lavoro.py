b45b2482e2fbdda2ebdaba02e3345a19364a1339
drutigliano11 <dennyrutigliano@gmail.com>
2024-07-11 13:52:42 +0200

    Created Menu Function.py

--- file_lavoro.py
+++ file_lavoro.py
@@ -7,3 +7,32 @@ client = MongoClient('mongodb+srv://maddalenabozzola_:1iEoHJKaFeMqZDHH@ufs13.p4d
 
 db = client['biglietti']
 #print(db)
+
+def menu():
+    while True:
+        print(f"""
+=================================================
+     
+    Benvenuta/o !!       
+ ______________________________________________
+|TROVA I TUOI CONCERTI PREFERITI               |
+|   a: Cerca per Artista                       |
+|   d: Cerca per Date                          |
+|   v: Cerca per Vicinanza                     |
+|______________________________________________|""")
+
+        scelta = input("\n> Inserisci la tua scelta (a, d, v): ").lower()
+        
+        match scelta:
+            
+            case "a":
+                print('A')
+                break
+            case "d":
+                print('D')
+                break
+            case "v":
+                print('V')
+                break
+            case _:
+                print("\n<<< Scelta non valida! Riprovare...")

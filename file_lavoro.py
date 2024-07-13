a71d05846122f60ec54e3da0513955379ece8b4d
Maddalena Bozzola <maddalena.bozzola@itsrizzoli.it>
2024-07-13 17:55:27 +0200

    ricerca evento, ricerca artista, anche parziali, sistemazione menu

--- file_lavoro.py
+++ file_lavoro.py
@@ -19,6 +19,7 @@ def menu():
 |   a: Cerca per Artista                       |
 |   d: Cerca per Date                          |
 |   v: Cerca per Vicinanza                     |
+|   e: Cerca per Evento                        |
 |______________________________________________|""")
 
         scelta = input("\n> Inserisci la tua scelta (a, d, v): ").lower()
@@ -26,7 +27,15 @@ def menu():
         match scelta:
             
             case "a":
-                print('A')
+                while True:
+                    a_ricerca = str(input('Inserisci il nome dell\'artista da cercare: ')).lower()
+                    a_valori = cerca_artista(a_ricerca)
+                    if a_valori:
+                        for a_valore in a_valori:
+                            print(a_valore)
+                        break
+                    else:
+                        print("Nessun evento trovato. Riprova.")
                 break
             case "d":
                 print('D')
@@ -34,6 +43,16 @@ def menu():
             case "v":
                 print('V')
                 break
+            case "e":
+                while True:
+                    e_ricerca = str(input('Inserisci il nome dell\'evento da cercare: ')).lower()
+                    e_valori = cerca_evento(e_ricerca)
+                    if e_valori:
+                        for e_valore in e_valori:
+                            print(e_valore)
+                        break
+                    else:
+                        print("Nessun evento trovato. Riprova.")
             case _:
                 print("\n<<< Scelta non valida! Riprovare...")
 
@@ -43,15 +62,23 @@ def main():
 if __name__ == "__main__":
     main()
     
-def cerca_artista(nome_ricerca):
+def cerca_artista(a_ricerca):
     artisti_cercati = db['artisti']
-    documenti_trovati = artisti_cercati.find(
-        {'artista': nome_ricerca},
+    a_documenti_trovati = artisti_cercati.find(   #$regex permette di trovare la parola anche se è una sottostringa del nome completo dell'artista
+        {'artista.nome_arte': {'$regex': a_ricerca}},
         {'_id': 0, 'artista.nome_arte': 1, 'lista_eventi': 1}
     )
-    valori = [d for d in documenti_trovati]
+    return list(a_documenti_trovati)
 
-    return valori
+#ricerca eventi, anche parziali con gestione di stringa inserita completamente errata
+def cerca_evento(e_ricerca):
+    eventi_cercati = db['eventi']
+    e_documenti_trovati = eventi_cercati.find(
+        {'nome_evento': {'$regex': e_ricerca}},
+        {'_id': 0, 'id_artisti': 0, 'data': 1, 'id_location': 0, 'biglietti.disponibili': 1, 'biglietti.prezzo': 1} 
+    )
+    return list(e_documenti_trovati)
+
     
 def cerca_per_vicinanza(lat, lon, distanza=7):
     raggio = distanza/6378.1 # conversione km to radianti
